f815e6050264e74da4f479a14b1459ad595cd009
auniquesun <Mr.sunhy@outlook.com>
2022-03-11 20:17:14 +0800

    training script

--- densenet/dist_one_node_train.sh
+++ densenet/dist_one_node_train.sh
@@ -1,7 +1,7 @@
 # distributed training on single node
 
 GPUS=$1
-local_rank=$2
+FREQ=$2
 
 python -m torch.distributed.launch --nproc_per_node=$GPUS \
-            main_single.py 
+            main_single.py --tensorboard --print_freq $FREQ
