f33f1d67eaa6c10d9ccd30ac3a51a89529263591
auniquesun <Mr.sunhy@outlook.com>
2022-03-15 09:29:24 +0800

    update file

--- densenet/dist_one_node_train.sh
+++ densenet/dist_one_node_train.sh
@@ -2,6 +2,8 @@
 
 GPUS=$1
 FREQ=$2
+BATCH=$3
+DATASET_ROOT=$4
 
 python -m torch.distributed.launch --nproc_per_node=$GPUS \
-            main_single.py --tensorboard --print_freq $FREQ
+            main_single.py --tensorboard --print_freq $FREQ --batch_size $BATCH --dataset_root $DATASET_ROOT
